fca525616b2ead000be76fc89cef03b38a3e6e6d
Juliana-Madeira <juliwood1980@gmail.com>
2022-06-24 10:09:28 -0300

    responsive changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-// let toggleMenu = document.querySelector('.bar__button');
+let toggleMenu = document.querySelector('.bar__button');
 
-// toggleMenu.onClick = function(){
-//     let menu = document.querySelector('.navbar__list');
-//     menu.classList.toggle('menu__toggling')
-// }
+toggleMenu.onClick = function(){
+    let menu = document.querySelector('.navbar__list');
+    menu.classList.toggle('menu__toggling')
+}
