8de9931f6dc6b77c26865bdf101c8e568c146832
Juliana Madeira <juliwood1980@gmail.com>
2022-07-15 12:20:33 -0300

    done

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-let toggleMenu = document.querySelector('.bar__button');
-
-toggleMenu.onClick = function(){
-    let menu = document.querySelector('.navbar__list');
-    menu.classList.toggle('menu__toggling')
-}
